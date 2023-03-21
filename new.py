3410f7420195464d21ae9bf5a90b64c54663b698
16Brijesh10 <brijesh.peju@gmail.com>
2023-03-21 17:19:21 +0530

    added if and else

--- new.py
+++ new.py
@@ -1,4 +1,11 @@
 #Vanakkam daa maplaa
 
-def sat_hello():
-    print("Vankkam!!!!!!!!!!!!!!1")
+def sat_hello(a):
+    if a==1:
+        return("Vankkam!!!!!!!!!!!!!!1")
+    else:
+        return("poda")
+a=int(input())
+print(sat_hello(a))
+
+    
